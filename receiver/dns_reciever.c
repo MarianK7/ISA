b1ebf60a2383ac54fff505284ce8a9a4635ce0ad
MarianK7 <marian.keszi@gmail.com>
2022-11-11 22:26:43 +0100

    server is now communicating with client

--- receiver/dns_reciever.c
+++ receiver/dns_reciever.c
@@ -7,32 +7,39 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <arpa/inet.h>
-#include <netinet/in.h> 
+#include <netinet/in.h>
 #include "dns_receiver_events.h"
+
+#define BUFFER 1024 // buffer length
+#define PORT 20004 // port number
 
 int main(int argc, char **argv)
 {
     int sockfd;
     char buffer[1024];
-    char *hello = "Hello from server";
     struct sockaddr_in servaddr, cliaddr;
+    socklen_t length;
 
     char *Base_Host;
     char *DST_Dirpath;
 
-    if(argc < 3){
+    if (argc < 3)
+    {
         printf("Wrong parameters \n");
         return 1;
-    }else if (argc > 4){
+    }
+    else if (argc > 4)
+    {
         printf("Too many parameters \n");
         return 1;
-    }else
+    }
+    else
     {
         Base_Host = argv[1];
         DST_Dirpath = argv[2];
         printf("Base_Host: %s \n Filepath: %s \n", Base_Host, DST_Dirpath);
     }
-    
+
     // Creating socket file descriptor
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
@@ -46,31 +53,39 @@ int main(int argc, char **argv)
     // Filling server information
     servaddr.sin_family = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(8080);
+    servaddr.sin_port = htons(PORT);
 
     // Bind the socket with the server address
-    if (bind(sockfd, (const struct sockaddr *)&servaddr,
-             sizeof(servaddr)) < 0)
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    int n, len;
+    int n, r;
 
-    len = sizeof(cliaddr); //len is value/resuslt
+    length = sizeof(cliaddr); // len is value/resuslt
 
-    n = recvfrom(sockfd, (char *)buffer, 1024,
-                 MSG_WAITALL, (struct sockaddr *)&cliaddr,
-                 &len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
+    while ((n = recvfrom(sockfd, buffer, BUFFER, 0, (struct sockaddr *)&cliaddr, &length)) >= 0)
+    {
+        printf("data received from %s, port %d\n", inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port));
 
-    sendto(sockfd, (const char *)hello, strlen(hello),
-           MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
-           len);
-    printf("Hello message sent.\n");
+        for (r = 0; r < n; r++)
+            if (islower(buffer[r]))
+                buffer[r] = toupper(buffer[r]);
+            else if (isupper(buffer[r]))
+                buffer[r] = tolower(buffer[r]);
+
+        r = sendto(sockfd, buffer, n, 0, (struct sockaddr *)&cliaddr, length); // send the answer
+        if (r == -1)
+            err(1, "sendto()");
+        else if (r != n)
+            errx(1, "sendto(): Buffer written just partially");
+        else
+            printf("data \"%.*s\" sent to %s, port %d\n", r - 1, buffer, inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+    }
+    printf("closing socket\n");
+    //close(sockfd);
 
     return 0;
-
 }
--- sender/dns_sender.c
+++ sender/dns_sender.c
@@ -7,23 +7,34 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <arpa/inet.h>
-#include <netinet/in.h> 
+#include <netinet/in.h>
+#include <netdb.h>
+#include <err.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include "dns_sender_events.h"
+
+#define BUFFER 1024 // buffer length
+#define PORT 20004   // port number
 
 int main(int argc, char **argv)
 {
 
     int sockfd;
-    struct sockaddr_in servaddr;
-    char buffer[1024];
-    char *hello = "Hello from client";
+    int msg_size, i;
+    struct sockaddr_in server, cliaddr; // address structures of the server and the client
+    socklen_t len, cliaddrLen;
+    char buffer[BUFFER];
 
     char *DNS_Server;
+    bool ns = false;    // control variable if the nameserver was set by parameter or not
+    bool input = false; // control variable if the input was set by parameter or not
     char *Base_Host;
     char *DST_Filepath;
     char *SRC_Filepath;
+    FILE* fp;
 
-    if (argc < 2)
+    if (argc < 3)
     {
         printf("Wrong parameters");
         return 1;
@@ -37,19 +48,22 @@ int main(int argc, char **argv)
     {
         if (argc == 3)
         {
-            Base_Host = argv[2];
-            DST_Filepath = argv[3];
+            Base_Host = argv[1];
+            DST_Filepath = argv[2];
+            printf("Base_Host: %s \n DST_Filepath: %s \n", Base_Host, DST_Filepath);
         }
         else if (argc == 4)
         {
-            Base_Host = argv[2];
-            DST_Filepath = argv[3];
-            SRC_Filepath = argv[4];
+            Base_Host = argv[1];
+            DST_Filepath = argv[2];
+            SRC_Filepath = argv[3];
+            input = true;
         }
         else if (argc == 5)
         {
             if (strcmp(argv[1], "-u") == 0)
             {
+                ns = true;
                 DNS_Server = argv[2];
                 Base_Host = argv[3];
                 DST_Filepath = argv[4];
@@ -65,9 +79,12 @@ int main(int argc, char **argv)
         {
             if (strcmp(argv[1], "-u") == 0)
             {
+                ns = true;
+                input = true;
                 DNS_Server = argv[2];
                 Base_Host = argv[3];
                 DST_Filepath = argv[4];
+                SRC_Filepath = argv[5];
                 printf("DNS Server: %s \n Base_Host: %s \n Filepath: %s \n", DNS_Server, Base_Host, DST_Filepath);
             }
             else
@@ -78,30 +95,69 @@ int main(int argc, char **argv)
         }
     }
 
-    // Creating socket file descriptor
+    if (ns == false)
+    {
+        char Nameserver[1024];
+        FILE *fpp;
+        fpp = popen("cat /etc/resolv.conf | grep 'nameserver' | awk -F ' ' '{print $2}'", "r");
+        fgets(Nameserver, sizeof(Nameserver), fpp);
+        fclose(fpp);
+        printf("Nameserver: %s \n", Nameserver);
+        DNS_Server = Nameserver;
+        printf("DNS Server: %s \n", DNS_Server);
+    }
+
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
+    printf("* Server socket created %i\n", sockfd);
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr(DNS_Server);
 
-    // Filling server information
-    servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(8080);
+    len = sizeof(server);
+    cliaddrLen = sizeof(cliaddr);
 
-    int n, len;
+    if (input == true)
+    {
+        fp = fopen(SRC_Filepath, "r");
+        if (fp == NULL)
+        {
+            printf("Could not open file %s", SRC_Filepath);
+            return 1;
+        }
+    }else
+    {
+        fp = stdin;
+    }
 
-    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-    printf("Hello message sent.\n");
+    while ((msg_size = read(STDIN_FILENO, buffer, BUFFER)) > 0)
+    // read input data from STDIN (console) until end-of-line (Enter) is pressed
+    // when end-of-file (CTRL-D) is received, n == 0
+    {
+        printf("* Sending message to server\n");
+        i = sendto(sockfd, buffer, msg_size, MSG_CONFIRM, (const struct sockaddr *)&server, len); // send data to the server
+        printf("* Data sent from %s, port %d (%d) to %s, port %d (%d)\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port), cliaddr.sin_port, inet_ntoa(server.sin_addr), ntohs(server.sin_port), server.sin_port);
 
-    n = recvfrom(sockfd, (char *)buffer, 1024, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
+        // read the answer from the server)
+        i = recvfrom(sockfd, (char *)buffer, BUFFER, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+        buffer[i] = '\0';
+        // printf("Client : %s\n", buffer);
 
+        printf("* UDP packet received from %s, port %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+        printf("%.*s", i, buffer); // print the answer
+    }
+    // reading data until end-of-file (CTRL-D)
+
+    if (msg_size == -1)
+        err(1, "reading failed");
+    if (input == true)
+        fclose(fp);
     close(sockfd);
-
+    printf("* Closing the client socket ...\n");
     return 0;
 }
